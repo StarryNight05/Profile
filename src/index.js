d8865821b0c1cdb95c178be254f60613605be5d0
Bryant1356 <bryantv0.2.0@gmail.com>
2023-06-11 21:50:33 +0700

    cek

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './App';
-import axios from "axios";
 
-axios.defaults.withCredentials = true;
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
 );
