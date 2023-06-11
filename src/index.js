ba70f3b072f6c3078a7369c6caa20f2c7b594edf
Bryant1356 <bryantv0.2.0@gmail.com>
2023-06-11 21:29:11 +0700

    cek

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
 import App from './App';
 import axios from "axios";
 
@@ -12,8 +10,6 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
