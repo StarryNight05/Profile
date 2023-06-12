d2c7cfbc2457c6a787a40d86304bc35d8718f673
Bryant1356 <bryantv0.2.0@gmail.com>
2023-06-12 22:26:04 +0700

    tes deploy

--- src/views/layout/Layout.jsx
+++ src/views/layout/Layout.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
 
 const Layout = ({ children }) => {
     return (
         <>
-            <Navbar />
             <div className="main" style={{ minHeight: '100vh' }}>
                 <main>
                     {children}
