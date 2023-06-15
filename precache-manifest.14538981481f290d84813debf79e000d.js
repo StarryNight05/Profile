f6409585c6ebb3d94b82456e65006134e95c9aa9
Bryant1356 <bryantv0.2.0@gmail.com>
2023-06-15 15:06:13 +0700

    Updates

---
+++ precache-manifest.14538981481f290d84813debf79e000d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f716f7e854d66f5377c",
+    "url": "/profile/static/js/0.5f716f7e.chunk.js"
+  },
+  {
+    "revision": "ba91ed6a17cccef3ba1d",
+    "url": "/profile/static/js/main.ba91ed6a.chunk.js"
+  },
+  {
+    "revision": "49e2eaa367ce24612c93",
+    "url": "/profile/static/js/2.49e2eaa3.chunk.js"
+  },
+  {
+    "revision": "8007fb4726b9024d35c0",
+    "url": "/profile/static/js/3.8007fb47.chunk.js"
+  },
+  {
+    "revision": "2ed71fb2cf1dcd1bf18b",
+    "url": "/profile/static/js/4.2ed71fb2.chunk.js"
+  },
+  {
+    "revision": "b9b1f4759c237f4e6490",
+    "url": "/profile/static/js/5.b9b1f475.chunk.js"
+  },
+  {
+    "revision": "401ffde58f3858b5a5e0",
+    "url": "/profile/static/js/6.401ffde5.chunk.js"
+  },
+  {
+    "revision": "71db2d653284265cce53",
+    "url": "/profile/static/js/7.71db2d65.chunk.js"
+  },
+  {
+    "revision": "e13e43f53990eb6c596a",
+    "url": "/profile/static/js/8.e13e43f5.chunk.js"
+  },
+  {
+    "revision": "050fd11b411e3b60f5da",
+    "url": "/profile/static/js/runtime~main.050fd11b.js"
+  },
+  {
+    "revision": "743c1b4cab3d0c10a9351b77c2ab07d9",
+    "url": "/profile/static/media/profile.743c1b4c.png"
+  },
+  {
+    "revision": "b7242574b0045a343111c29dc780c4c8",
+    "url": "/profile/static/media/pkl.b7242574.png"
+  },
+  {
+    "revision": "53b7b1944850a221354abda32ba4948c",
+    "url": "/profile/static/media/spp.53b7b194.png"
+  },
+  {
+    "revision": "943be19f8e078ea8a6092e2eca8a9e35",
+    "url": "/profile/static/media/evote.943be19f.png"
+  },
+  {
+    "revision": "812d6b6da0b7d3dc812e24a9c476d041",
+    "url": "/profile/index.html"
+  }
+];
